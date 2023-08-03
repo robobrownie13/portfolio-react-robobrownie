caf67c33b0d5bc2df1bb77b2a46017466fbd7bbf
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2023-08-03 11:55:16 -0500

    added About style

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,19 @@
+import "../styles/About.css";
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="about-container">
+      <h2 className="about-title">About Me</h2>
+      <p className="about-text">
+        I am a current student developer enrolled at Rice University's Full Flex
+        Coding Bootcamp. In May of 2022, I decided to transition fields from
+        music education to software development. I spent several months going
+        over open source documentation about HTML, CSS, and Javascript. The
+        follow projects showcase a mix of work I have published from my current
+        bootcamp.
+      </p>
+    </div>
+  );
 }
 
 export default About;
