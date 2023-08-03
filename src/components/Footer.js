f8581ba75b938dcaad67380ed5dc0626d54d615d
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2023-08-03 00:32:52 -0500

    added component files

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div>
+      <a href="mailto: easorj@gmail.com">Email</a>
+      <a href="https://github.com/robobrownie13">Github</a>
+      <a href="https://www.linkedin.com/in/rojan-easo-792a70254/">LinkedIn</a>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Main;
