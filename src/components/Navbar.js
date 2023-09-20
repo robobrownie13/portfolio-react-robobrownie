63c9463ce038f60291dffaa7b69997fbaa83935e
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2023-09-20 12:44:11 -0500

    adjusted navbar to BEM format and added some styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,30 +3,30 @@ import "../styles/Navbar.css";
 const Navbar = ({ handleNavClick }) => {
   return (
     <div className="navigation">
-      <input type="checkbox" class="navigation_checkbox" id="navi-toggle" />
-      <label for="navi-toggle" class="navigation_button">
+      <input type="checkbox" class="navigation__checkbox" id="navi-toggle" />
+      <label for="navi-toggle" class="navigation__button">
         <span class="navigation_icon">&nbsp;</span>
       </label>
-      <div class="navigation_background">&nbsp;</div>
+      <div class="navigation__background">&nbsp;</div>
 
-      <nav class="navigation_nav">
-        <ul class="navigation_list">
-          <li class="navigation_item">
+      <nav class="navigation__nav">
+        <ul class="navigation__list">
+          <li class="navigation__item">
             <a href="#home" onClick={() => handleNavClick("home")}>
               Home
             </a>
           </li>
-          <li class="navigation_item">
+          <li class="navigation__item">
             <a href="#about" onClick={() => handleNavClick("about")}>
               About
             </a>
           </li>
-          <li class="navigation_item">
+          <li class="navigation__item">
             <a href="#portfolio" onClick={() => handleNavClick("portfolio")}>
               Portfolio
             </a>
           </li>
-          <li class="navigation_item">
+          <li class="navigation__item">
             <a href="#contact" onClick={() => handleNavClick("contact")}>
               Contact
             </a>
